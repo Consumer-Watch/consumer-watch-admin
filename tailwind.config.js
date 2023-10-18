9f09a2c3dfd14974041a05cc7fa8f5102fb6df04
fortune710 <fortune0208@yahoo.com>
2023-10-18 14:07:33 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary': 'var(--primary)',
+        'secondary': 'var(--secondary)',
+        'background': 'var(--background)',
+        'hover':'var(--secondary-hover)',
+        'box-shadow':'var(--secondary-box-shadow)',
+      },
+    },
+  },
+  plugins: [],
+}
---
+++ utils/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Josefin_Sans } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'], weight: "500", variable: "--font-inter" });
+export const josefin = Josefin_Sans({ weight: "400", subsets: ["latin"], variable: "--font-josefin" });
